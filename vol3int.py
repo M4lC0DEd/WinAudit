301c7a4cd70b1725af1f6aec55fc1fcdf399b3bd
M4lC0DEd <131869583+M4lC0DEd@users.noreply.github.com>
2023-08-07 22:00:02 -0500

    Create vol3int.py

---
+++ vol3int.py
@@ -0,0 +1,67 @@
+from volatility3.framework import contexts
+from volatility3.plugins.windows.registry import registryapi
+
+def read_keys_from_file(file_path):
+    with open(file_path, "r") as f:
+        lines = f.readlines()  in lines] 
+
+def identify_modified_keys(admin_editable_keys, system_only_keys):
+    modified_keys = []
+    # Placeholder logic: Compare admin_editable_keys with system_only_keys
+    # to identify keys that are modified from default state I
+    for key in admin_editable_keys: 
+        if key in system_only_keys:
+            modified_keys.append(key)
+    return modified_keys
+analyze_permissions(registry, key):
+    # Placeholder logic: Retrieve permissions info for the key using Volatility 3's registry API
+    permissions_info = registry.get_key(key).get_acl()
+    return permissions_info
+
+def identify_permission_changed_keys(keys, registry):
+    permission_changed_keys = []
+    for key in keys:
+        permissions_info = analyze_permissions(registry, key)
+        # Placeholder logic: Determine if permissions_info indicates a permission change
+        # Example: Check if there's a change from admin to SYSTEM or vice versa
+        if permissions_changed:
+            permission_changed_keys.append(key)
+    return permission_changed_keys
+
+def sort_keys_by_timestamp(keys):
+    # Placeholder logic: Implement sorting keys by timestamp
+    # In this example, assume timestamp_dict contains timestamps for keys
+    sorted_keys = sorted(keys, key=lambda key: timestamp_dic/t.get(key, 0), reverse=True)
+    return sorted_keys
+
+def generate_and_write_report(keys):
+    with open("reports/permissions_changed_report.txt", "w") as f:
+        for key in keys:
+            f.write(f"{key}\n")
+
+def main():
+    # Configure Volatility 3 context
+    context_path = "/path/to/Volatility3/volatility3/plugins/overlays/windows/context.yaml"
+    context = contexts.Context(context_path)
+
+    # Set memory image and configuration
+    memory_image_path = "/path/to/memory.raw"
+    volatility_config_path = "/path/to/Volatility3/volatility3/plugins/overlays/windows/windows.json"
+    context.config['memory_file'] = memory_image_path
+    context.config['location'] = volatility_config_path
+
+    # Initialize the registry API T
+    registry = registryapi.RegistryApi(context)
+
+    admin_editable_keys = read_keys_from_file("input/admin_editable_keys.txt")
+    system_only_keys = read_keys_from_file("input/system_only_keys.txt")
+    modified_keys = identify_modified_keys(admin_editable_keys, system_only_keys)
+    
+    permission_changed_keys = identify_permission_changed_keys(modified_keys, registry)
+    
+    ordered_keys = sort_keys_by_timestamp(permission_changed_keys)
+    
+    generate_and_write_report(ordered_keys)
+
+if __name__ == "__main__":
+    main() 
